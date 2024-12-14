3e123aeab910fd363591a82db1c5414e3899e28e
Cyrus-11 <ayomidececil.it@gmail.com>
2024-12-14 13:12:08 +0100

    frontend

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    window.location.href = '/api/students/login';
+  });
+  
+  document.getElementById('registerButton').addEventListener('click', () => {
+    window.location.href = '/api/students/register';
+  });
+  
